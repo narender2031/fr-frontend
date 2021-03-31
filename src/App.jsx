fbd245478ce0fd836f86058bf28457c27d82099d
Narender Singh <narender2031@gmail.com>
2021-03-31 10:06:59 +0530

    intial code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+import Nav from './components/layout/Nav';
+import './App.scss';
+
+function App() {
+  return (
+    <Container>
+      <Nav />
+    </Container>
+  );
+}
+
+export default App;
